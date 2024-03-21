678582d20e3334d058d4e23a463840e00e9de1f5
Boyd Querubin <boyd@ezlynkna.com>
2024-03-21 11:12:24 -0600

    update

---
+++ practice1.js
@@ -0,0 +1,57 @@
+// if (0 > 5) {
+//     console.log('big number');
+// } else {
+//     console.log('small number')
+// }
+
+//command lines in terminal
+//ls, cd, pwd, mv, touch, cat
+
+// let name = "bruv";
+// let greeting = "wattup";
+
+// let nameGreet = greeting + name;
+
+// console.log(nameGreet);
+
+// let johnAge = 28
+// let robertAge = 30
+
+// if (johnAge < robertAge) {
+//     console.log(`John's age is ${johnAge} and Robert's age is ${robertAge} therefore, Robert is older!`);
+// } else if (johnAge > robertAge) {
+//     console.log(`John's age is ${johnAge} and Robert's age is ${robertAge} therefore, John is older!`);
+// } else {
+//     console.log("John and Robert are the same age!");
+// }
+
+// alphabet("abcdefghijklmnop")
+
+// const practiceForLoops = ["I need to practice For Loops"];
+// const repeatSentence = 5;
+
+// for(let i = 0; i < repeatSentence; i++) {
+//     console.log(practiceForLoops);
+// }
+
+// function vowelsAndConsonants(string) {
+//     const vowels = ["a", "e", "i", "o", "u"];
+//     for(let char of string) {
+//         if(vowels.includes(char)) {
+//             console.log(char);
+//         }
+//     }
+//     for(let char of string) {
+//         if(!vowels.includes(char)) {
+//             console.log(char);
+//         }
+//     }
+// }
+
+// vowelsAndConsonants("kdhoawkdakecmapcpw");
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// console.log(sum(1, 2));
